f29036c6317e78356b57949f39eab5c0fa26c1c2
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-20 15:06:21 +0530

    Create default.vue: Default layout of the app

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+import AppHeader from "~/components/utils/AppHeader.vue";
+import AppFooter from "~/components/utils/AppFooter.vue";
+</script>
+
+<template>
+    <AppHeader/>
+    <slot/>
+    <AppFooter/>
+</template>
+
+<style scoped>
+
+</style>
