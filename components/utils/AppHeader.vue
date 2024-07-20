f6fcaeb7c599bb2e8c207610e9b9505ef1ea1aec
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-20 15:07:49 +0530

    Create AppHeader.vue: Header component for the app

---
+++ components/utils/AppHeader.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<header class="font-trebuchet">
+    <div class="grid lg:grid-cols-4 place-items-center place-content-center">
+        <div></div>
+        <ul class="flex justify-around items-center text-lg text-gray-800 dark:text-gray-100 w-full col-span-2 py-2">
+            <li class="hover:text-primary cursor-pointer">Home</li>
+            <li class="hover:text-primary cursor-pointer">Services</li>
+            <li class="hover:text-primary cursor-pointer">About</li>
+            <li class="hover:text-primary cursor-pointer">Blogs</li>
+        </ul>
+        <div></div>
+    </div>
+</header>
+</template>
+
+<style scoped>
+
+</style>
