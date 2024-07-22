6881a2233991ae27f8b8739450558f8dbd071651
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-22 23:00:56 +0530

    Create AppHero: Hero Section for the Portfolio

---
+++ components/home/AppHero.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const items: string[] = [
+    "Software Developer",
+    "Web Developer",
+    "Data Scrapper",
+    "Android Developer"
+];
+
+const view = ref<HTMLElement>();
+
+onMounted(() => {
+    if(view) {
+        console.log(view.value)
+    }
+})
+</script>
+
+<template>
+<section>
+    <div class="h-[90vh] text-center flex flex-col items-center justify-center font-trebuchet text-center">
+        <h1 class="text-8xl">
+            I'm <span class="text-primary">Ayush Jaiswal</span>
+        </h1>
+        <span ref="view"></span>
+    </div>
+</section>
+</template>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 
+import AppHero from "~/components/home/AppHero.vue";
 </script>
 
 <template>
-
+    <AppHero/>
 </template>
 
 <style scoped>
