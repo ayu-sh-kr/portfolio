e179f06710513a4ff420559d11abc9bf90f94342
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-24 12:29:38 +0530

    Update index.vue: Added component to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 
 import AppHero from "~/components/home/AppHero.vue";
+import AppAbstract from "~/components/home/AppAbstract.vue";
 </script>
 
 <template>
     <AppHero/>
+    <AppAbstract/>
 </template>
 
 <style scoped>
