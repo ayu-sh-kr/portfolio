05ec1ca2938824cd8ffedf6e4785f2245f2a9c20
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-25 15:19:13 +0530

    Update index.vue: Added the Section AboutMe.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,13 @@
 
 import AppHero from "~/components/home/AppHero.vue";
 import AppAbstract from "~/components/home/AppAbstract.vue";
+import AboutMe from "~/components/home/AboutMe.vue";
 </script>
 
 <template>
     <AppHero/>
     <AppAbstract/>
+    <AboutMe/>
 </template>
 
 <style scoped>
