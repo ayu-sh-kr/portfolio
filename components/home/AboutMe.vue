c8352be04a10c8f42910a59aa16f3e1d5f49beed
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-15 22:55:11 +0530

    Update AboutMe.vue: Layout Redesign and content enhancement

--- components/home/AboutMe.vue
+++ components/home/AboutMe.vue
@@ -4,25 +4,30 @@ import SectionHeader from "~/components/utils/SectionHeader.vue";
 </script>
 
 <template>
-    <section class="px-5 py-10 font-trebuchet h-screen">
+    <section class="py-10 font-trebuchet max-w-7xl mx-auto">
 
         <SectionHeader secondary="Get To Know More" primary="About Me" />
 
-        <div class="py-20 grid sm:grid-cols-2 grid-cols-1 h-[90%] place-items-center">
-            <div class="text-center">
-                <h1 class="lg:text-4xl md:text-3xl text-2xl text-center tracking-wide leading-normal">Software Engineer and Life Long Learner</h1>
-                <div class="flex gap-x-5 justify-center gap-y-3">
-                    <UBadge label="Backend Developer" color="fuchsia" variant="soft"/>
-                    <UBadge label="Frontend Developer" color="amber" variant="soft"/>
-                    <UBadge label="Data Scrapper" color="emerald" variant="soft"/>
-                    <UBadge label="Android Developer" color="blue" variant="soft"/>
+        <div class="py-20 grid sm:grid-cols-2 grid-cols-1 place-items-center">
+            <div class="text-left">
+                <div class="w-full flex flex-col items-start lg:text-8xl md:text-6xl text-5xl">
+                    <h1 class="text-left text-wrap w-full md:w-2/3 h-fit font-semibold bg-clip-text text-transparent bg-gradient-to-r from-purple-600 via-purple-400 to-purple-300">Learn</h1>
+                    <h1 class="text-left text-wrap w-full md:w-2/3 h-fit font-semibold bg-clip-text text-transparent bg-gradient-to-r from-purple-600 via-purple-400 to-purple-300">Build</h1>
+                    <h1 class="text-left text-wrap w-full md:w-2/3 h-fit font-semibold bg-clip-text text-transparent bg-gradient-to-r from-purple-600 via-purple-400 to-purple-300">Deploy</h1>
                 </div>
                 <p class="md:text-lg text-sm text-gray-800 dark:text-gray-200 py-5 font-extralight">
-                    Hello there, I'm <strong class="text-primary font-semibold">Ayush Jaiswal</strong>, Software Engineer, Developing Web Apps, Android Apps and Data Scrapper based in <strong class="text-primary font-semibold">Varanasi, India</strong>.
-                    I specialize in Java and Spring Boot. I use Kotlin Multiplatform using Compose for <span class="text-primary font-semibold">Cross Platform</span> mobile app development.
+                    Hello there, I'm <strong class="text-primary font-semibold">Ayush</strong>, Software Engineer based in <strong class="text-primary font-semibold">Varanasi, India</strong>.
                     <br>
-                    I help companies in building well designed, easy to maintain and fast backend applications.
+                    I help companies to build better web applications and to improve their knowledge, workflows, and culture.
                 </p>
+
+                <p>I specialize in Java and Spring Boot. I use Kotlin Multiplatform using Compose for <span class="text-primary font-semibold">Cross Platform</span> mobile app development.</p>
+
+                <div class="grid grid-cols-2 pt-10 text-xl place-items-start">
+                    <button class="bg-gradient-to-r from-purple-400 via-purple-500 to-purple-600 py-1 px-3 text-white md:w-2/3 w-full">SCHEDULE MEETING</button>
+                    <button class="py-1 px-3 dark:text-white md:w-2/3 w-full border-4 border-purple-600">ABOUT ME</button>
+                </div>
+
             </div>
             <div class="flex justify-center items-center h-full">
                 <div class="lg:h-96 lg:w-96 md:h-80 md:w-80 h-72 w-72 bg-emerald-800 rounded-full"></div>
