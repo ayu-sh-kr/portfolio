dae99e761cc91e638a8cc48f7d46912ea8438a49
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-15 22:54:32 +0530

    Update index.vue: removed unused import

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,6 @@
 import AppHero from "~/components/home/AppHero.vue";
 import AppAbstract from "~/components/home/AppAbstract.vue";
 import AboutMe from "~/components/home/AboutMe.vue";
-import MeteoriteAnimation from "~/components/animation/meteorite/MeteoriteAnimation.vue";
 </script>
 
 <template>
