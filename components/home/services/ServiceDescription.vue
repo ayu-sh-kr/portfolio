f5a70e911d77d6c976abe92327d0fa4f505b0220
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-19 23:16:01 +0530

    Create ServiceDescription.vue: component to render description of the Service cards

---
+++ components/home/services/ServiceDescription.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps({
+    description: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+<p class="text-gray-600 dark:text-gray-400">
+    {{description}}
+</p>
+</template>
+
+<style scoped>
+
+</style>
