2f0403ee7d16e50fb64e738d9c10bd7abf5fd923
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-19 23:14:41 +0530

    Create ServiceHeader.vue: Contains UI for header of the Service card

---
+++ components/home/services/ServiceHeader.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props = defineProps({
+    text: {
+        type: String,
+        required: true
+    }
+});
+</script>
+
+<template>
+    <h3 class="text-2xl font-semibold text-dark">{{ text }}</h3>
+</template>
+
+<style scoped>
+
+</style>
