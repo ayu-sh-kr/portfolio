d94e06dc82dce7d78e54089670c257f88eeb3f99
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-19 23:15:25 +0530

    Create AppService.vue: Section to shows services offered

---
+++ components/home/AppServices.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+import Scaffold from "~/components/utils/Scaffold.vue";
+import ServiceScaffold from "~/components/home/services/ServiceScaffold.vue";
+import ServiceHeader from "~/components/home/services/ServiceHeader.vue";
+import ServiceDescription from "~/components/home/services/ServiceDescription.vue";
+</script>
+
+<template>
+    <Scaffold class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <ServiceScaffold>
+            <ServiceHeader text="Consulting"/>
+            <ServiceDescription description="Companies reach out to help them build better Vue and Nuxt applications - from solving problems to discussing the right abstractions and architecture."/>
+        </ServiceScaffold>
+    </Scaffold>
+</template>
+
+<style scoped>
+
+</style>
