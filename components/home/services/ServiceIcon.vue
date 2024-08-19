a8738f6f625485a591c99d7ae30468548496a38a
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-20 00:00:42 +0530

    Create ServiceIcon.vue: Abstract away the Nuxt Icon component providing with default design and transition

---
+++ components/home/services/ServiceIcon.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    icon: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <Icon dynamic :name="`i-${icon}`" class="text-primary text-6xl text-center font-semibold transition-all group-hover:scale-125"/>
+</template>
+
+<style scoped>
+
+</style>
