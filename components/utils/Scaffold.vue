22e3580d9283f3cbc151d60a7768ace0adde2f47
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-19 23:13:41 +0530

    Create Scaffold.vue: Scaffold is repeating component mapped to section in html that provides default size and padding and font to the sections

---
+++ components/utils/Scaffold.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    class: {
+        type: String,
+        required: false
+    }
+})
+</script>
+
+<template>
+    <section class="py-10 font-trebuchet max-w-7xl mx-auto"
+             :class="props.class"
+    >
+        <slot/>
+    </section>
+</template>
+
+<style scoped>
+
+</style>
