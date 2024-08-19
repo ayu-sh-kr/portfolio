d063705c732790fda21fa93d97b3d88f1b795ba7
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-20 00:01:34 +0530

    Update ServiceScaffold.vue: Configured the layout for content and container both horizontally and vertically

--- components/home/services/ServiceScaffold.vue
+++ components/home/services/ServiceScaffold.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <div class="grid grid-cols-1 space-y-3 text-center place-content-center">
+    <div class="grid grid-cols-1 space-y-3 text-center place-content-start place-items-center group transition-all duration-500">
         <slot/>
     </div>
 </template>
