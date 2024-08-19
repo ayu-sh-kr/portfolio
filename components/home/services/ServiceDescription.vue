18beeeedff86a275dfb9c9ac5d6f490cffe9dc8e
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-20 00:01:47 +0530

    Update ServiceDescription.vue

--- components/home/services/ServiceDescription.vue
+++ components/home/services/ServiceDescription.vue
@@ -3,12 +3,17 @@ const props = defineProps({
     description: {
         type: String,
         required: true
+    },
+
+    size: {
+        type: String,
+        required: false
     }
 })
 </script>
 
 <template>
-<p class="text-gray-600 dark:text-gray-400">
+<p :class="`${size ?? 'text-lg'} text-gray-600 dark:text-gray-400`">
     {{description}}
 </p>
 </template>
