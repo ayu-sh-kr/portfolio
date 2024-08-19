906edc9fccbcc9ff1adce47c7f91740b890ea31d
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-19 23:14:14 +0530

    Create ServiceScaffold.vue: Card component for service data, provide common ui for all the cards

---
+++ components/home/services/ServiceScaffold.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="grid grid-cols-1 space-y-3 text-center place-content-center">
+        <slot/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
