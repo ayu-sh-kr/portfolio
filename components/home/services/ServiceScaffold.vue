9dbeab3caec9077421ad904cc1d6349a98b69cdf
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-20 23:22:51 +0530

    Update ServiceScaffold.vue

--- components/home/services/ServiceScaffold.vue
+++ components/home/services/ServiceScaffold.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <div class="grid grid-cols-1 space-y-3 text-center place-content-start place-items-center group transition-all duration-500">
+    <div class="grid grid-cols-1 space-y-3 text-center place-content-start place-items-center group transition-all duration-500 px-3 md:px-5">
         <slot/>
     </div>
 </template>
