6a8a3d2230b784dfe3f20dc092005fda43bf0dbd
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-20 23:21:46 +0530

    Create BigHeader.vue: Component for header on AboutMe.vue Section

---
+++ components/home/about/BigHeader.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    text: {
+        type: String,
+        required: true
+    }
+});
+
+</script>
+
+<template>
+    <h1 class="text-left text-wrap w-full md:w-2/3 h-fit font-semibold bg-clip-text text-transparent bg-gradient-to-r from-purple-600 via-purple-400 to-purple-300">
+        {{ text }}
+    </h1>
+</template>
+
+<style scoped>
+
+</style>
