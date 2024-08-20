fb5b5be1c940119579d1f837884ff11e431057dc
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-20 23:43:59 +0530

    Create Reactiveicon.vue: Logic to switch between variant of icon upon hover

---
+++ components/home/about/ReactiveIcon.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    icon1: {
+        type: String,
+        required: true
+    },
+
+    icon2: {
+        type: String,
+        required: true
+    },
+
+    class: {
+        type: String,
+        required: false
+    }
+});
+
+const icon = ref<string>(props.icon1);
+
+const resolveEnter = () => {
+    icon.value = props.icon2;
+}
+
+const resolveLeave = () => {
+    icon.value = props.icon1;
+}
+
+</script>
+
+<template>
+    <UIcon dynamic :name="`i-${icon}`" :class="`${props.class ?? 'text-4xl'}`"
+           @mouseenter="resolveEnter" @mouseleave="resolveLeave"
+    />
+</template>
+
+<style scoped>
+
+</style>
