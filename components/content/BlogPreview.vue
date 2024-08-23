4ea38e380bbb82b199eb5b7f25e7b59c84f926aa
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-23 21:18:27 +0530

    Create BlogPreview.vue: Component to give preview of the blog

---
+++ components/content/BlogPreview.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+import AppParagraph from "~/components/utils/AppParagraph.vue";
+import type {BlogPreview} from "~/app";
+import HashTag from "~/components/content/HashTag.vue";
+
+const props = defineProps({
+    blog: {
+        type: Object as PropType<BlogPreview>,
+        required: true
+    }
+});
+
+const dateTime = computed(() => {
+    const date = new Date(props.blog.publishedOn);
+    return date.toDateString() +  ' ' + date.toLocaleTimeString();
+})
+
+</script>
+
+<template>
+    <NuxtLink :to="blog._path" v-if="blog.published">
+        <div class="p-4 rounded-lg hover:bg-gray-200/50 dark:hover:bg-gray-800/40 bg-white dark:bg-gray-900 shadow-lg transition-all space-y-2 my-10">
+            <UBadge color="emerald" variant="soft">{{dateTime}}</UBadge>
+            <AppParagraph tag="h3" class="text-2xl font-semibold">{{ blog.title }}</AppParagraph>
+            <AppParagraph look="paragraph" class="group-hover:text-gray-200">{{ blog.description }}</AppParagraph>
+            <div class="flex gap-x-5">
+                <HashTag v-for="(topic, index) in blog.topics" :key="index" :tag="topic"/>
+            </div>
+        </div>
+    </NuxtLink>
+</template>
+
+<style scoped>
+
+</style>
