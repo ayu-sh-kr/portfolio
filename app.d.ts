30053cae73197aa98e6b6d467addd80ae3cfce8e
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-24 01:58:47 +0530

    Update app.d.ts: Added type for Styling tags

--- app.d.ts
+++ app.d.ts
@@ -14,3 +14,23 @@ declare type BlogBase = {
 declare type Blog = BlogBase & ParsedContent
 declare type ParsedContentPreview = Omit<ParsedContent, 'body' | 'excerpt'>
 declare type BlogPreview = BlogBase & ParsedContentPreview
+
+declare interface TagStyle {
+    h1: string,
+    h2: string,
+    h3: string,
+    h4: string,
+    h5: string,
+    p: string
+}
+
+declare interface TagBase {
+    base: string,
+    font: string
+}
+
+declare type Tag = TagBase & TagStyle
+
+declare interface AppStyle {
+    tag: Tag
+}
