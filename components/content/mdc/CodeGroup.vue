3cdd479c64b92f5e5ebd76023bcc0c3216b0c61a
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-24 02:01:07 +0530

    Update CodeGroup.vue: Change the styling

--- components/content/mdc/CodeGroup.vue
+++ components/content/mdc/CodeGroup.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-<div class="p-4 rounded-md bg-gray-200 dark:bg-gray-800 mt-3">
+<div class="p-4 rounded-md bg-slate-200 dark:bg-gray-800 mt-5 overflow-x-auto">
     <slot/>
 </div>
 </template>
