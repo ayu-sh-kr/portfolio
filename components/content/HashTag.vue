30e5d09959d327dfa0a5ca5f99fd0443c35a2d31
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-23 21:18:48 +0530

    Create HashTag.vue: Visualize tags on the blog

---
+++ components/content/HashTag.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+defineProps({
+    tag: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+<span class="text-primary font-light">#{{tag}}</span>
+</template>
+
+<style scoped>
+
+</style>
