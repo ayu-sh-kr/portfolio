baad0506586885fc1b7b953fd11d107574cc43a2
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-23 21:18:01 +0530

    Create BackButton.vue: Button to navigate back to blog page

---
+++ components/content/BackButton.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<NuxtLink to="/blogs">
+    <span class="flex items-center space-x-3 text-gray-600 dark:text-gray-400">
+        <UIcon dynamic name="i-material-symbols:keyboard-double-arrow-left" class="size-6"/>
+        <span class="hover:underline transition-all text-lg font-semibold">All Blogs</span>
+    </span>
+</NuxtLink>
+</template>
+
+<style scoped>
+
+</style>
