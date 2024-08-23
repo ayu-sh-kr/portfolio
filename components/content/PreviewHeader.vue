50f293a2f79dc2934654a6a44742acce073e661e
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-24 02:01:31 +0530

    Create PreviewHeader.vue: Custom header for Blog Preview

---
+++ components/content/PreviewHeader.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+defineProps({
+    text: {
+        type: String,
+        required: true
+    }
+})
+
+</script>
+
+<template>
+    <div>
+        <H4 class="relative inline-flex items-center before:content-[''] before:h-10 before:bg-gray-900/60  before:w-1 before:mr-4">{{ text }}</H4>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
