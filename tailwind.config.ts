00d7577b8c8916121e2f097436e3f7db4c8a81fc
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-24 11:17:34 +0530

    Update tailwind.config.ts: Update tailwind scopes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ import {type Config} from "tailwindcss"
 export default <Partial<Config>>{
   darkMode: 'class',
   content: [
+    ".{ts, js}",
     "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
