f59bd7a87374ea5387c42203f4b13689fed11a77
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-04 22:31:37 +0530

    Update Paragraph.vue
    Change the font size for responsiveness

--- components/content/tags/Paragraph.vue
+++ components/content/tags/Paragraph.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 
+import STYLE from "~/app.style";
+
 </script>
 
 <template>
-    <p class="text-dark mt-2 text-base sm:text-lg md:text-xl">
+    <p class="text-dark mt-2 text-base sm:text-lg">
         <slot/>
     </p>
 </template>
